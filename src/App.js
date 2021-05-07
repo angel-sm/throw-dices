a1e73a3e64b104c204cd4b63c775b42dd2eba652
josesanchez <josesanchez@solemti.mx>
2021-05-07 12:06:25 -0500

    Update: non repet numbers function

--- src/App.js
+++ src/App.js
@@ -3,19 +3,12 @@ import { Dice } from "./components/Dice";
 
 export const App = () => {
   const [turnAll, setTurnAll] = useState();
-  const [isTurn, setIsTun] = useState(true);
-
   return (
     <>
-      <Dice turnAll={turnAll} setIsTun={setIsTun} faces={6} />
-	    <Dice turnAll={turnAll} setIsTun={setIsTun} faces={6} />
-      <Dice turnAll={turnAll} setIsTun={setIsTun} faces={10} />
-
-      {isTurn ? (
-        <button onClick={() => setTurnAll((turnAll) => !turnAll)}>Turn</button>
-      ) : (
-        ""
-      )}
+      <Dice turnAll={turnAll} faces={6} />
+      <Dice turnAll={turnAll} faces={6} />
+      <Dice turnAll={turnAll} faces={10} />
+      <button onClick={() => setTurnAll((turnAll) => !turnAll)}>Turn</button>
     </>
   );
 };
--- src/components/Dice/index.js
+++ src/components/Dice/index.js
@@ -16,23 +16,28 @@ export const Dice = ({turnAll, faces}) => {
     
     const [turn, setTurn] = useState(false);    
     const [num, setNum] = useState(0);
+    
+    const generateNumber = () => {
+        const randomNum =  Math.floor(Math.random() * faces) + 1;
+        return num === randomNum ? generateNumber() : randomNum
+    }
 
-    const turnHandler = () => {
+    const turnDice = () => {
         const getNumber = new Promise((resolve, rejec) => {
             setTurn(turn => !turn);
             setTimeout(() => {
-                resolve(Math.floor(Math.random() * faces) + 1)
+                resolve(generateNumber())
             }, 2000);
         })
 
         getNumber.then(number => {
             setTurn(turn => !turn);
-            num === number ? turnHandler() : setNum(number)
+            setNum(number)
         })
     };
 
     useEffect(() => {
-        turnHandler()
+        turnDice()
     }, [turnAll])
 
     return(
